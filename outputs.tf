146c6e87cc07c0bd96dee2567cd417548e6bef77
Jakub Sokołowski <jakub@status.im>
2018-11-06 16:51:34 +0100

    add more outputs for the module

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,16 @@
+locals = {
+  public_ips = "${digitalocean_droplet.host.*.ipv4_address}"
+  hostnames  = "${digitalocean_droplet.host.*.name}"
+}
+
 output "public_ips" {
-  value = ["${digitalocean_droplet.host.*.ipv4_address}"]
+  value = ["${local.public_ips}"]
+}
+
+output "hostnames" {
+  value = ["${local.hostnames}"]
+}
+
+output "hosts" {
+  value = ["${zipmap(local.hostnames, local.public_ips)}"]
 }
