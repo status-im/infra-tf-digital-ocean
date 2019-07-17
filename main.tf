3d66c89a572b04287b020aac2c1b669576388337
Jakub Sokołowski <jakub@status.im>
2019-07-17 18:38:14 -0400

    use first class expressions
    https://github.com/hashicorp/terraform-guides/tree/master/infrastructure-as-code/terraform-0.12-examples/first-class-expressions
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- main.tf
+++ main.tf
@@ -1,28 +1,28 @@
 /* DERIVED --------------------------------------*/
 locals {
-  stage       = "${terraform.workspace}"
-  tokens      = "${split(".", local.stage)}"
+  stage       = terraform.workspace
+  tokens      = split(".", local.stage)
   dc          = "${var.provider_name}-${var.region}"
   /* tags for the dropplet */
-  tags        = ["${local.stage}", "${var.group}", "${var.env}"]
-  tags_sorted = "${sort(distinct(local.tags))}"
-  tags_count  = "${length(local.tags_sorted)}"
+  tags        = [local.stage, var.group, var.env]
+  tags_sorted = sort(distinct(local.tags))
+  tags_count  = length(local.tags_sorted)
   /* always add SSH, Tinc, Netdata, and Consul to allowed ports */
   open_ports  = concat(["22", "655", "8000", "8301"], var.open_ports)
 }
 /* RESOURCES ------------------------------------*/
 
 resource "digitalocean_tag" "host" {
-  name  = "${element(local.tags_sorted, count.index)}"
-  count = "${local.tags_count}"
+  name  = element(local.tags_sorted, count.index)
+  count = local.tags_count
 }
 
 /* Optional resource when vol_size is set */
 resource "digitalocean_volume" "host" {
   name      = "data.${var.name}-${format("%02d", count.index+1)}.${local.dc}.${var.env}.${local.stage}"
-  region    = "${var.region}"
-  size      = "${var.vol_size}"
-  count     = "${var.vol_size > 0 ? var.host_count : 0}"
+  region    = var.region
+  size      = var.vol_size
+  count     = var.vol_size > 0 ? var.host_count : 0
   lifecycle {
     prevent_destroy = true
     /* We do this to avoid destrying a volume unnecesarily */
@@ -33,13 +33,13 @@ resource "digitalocean_volume" "host" {
 resource "digitalocean_droplet" "host" {
   name   = "${var.name}-${format("%02d", count.index+1)}.${local.dc}.${var.env}.${local.stage}"
 
-  image  = "${var.image}"
-  region = "${var.region}"
-  size   = "${var.size}"
-  count  = "${var.host_count}"
+  image    = var.image
+  region   = var.region
+  size     = var.size
+  count    = var.host_count
+  ssh_keys = var.ssh_keys
 
   tags   = digitalocean_tag.host[*].id
-  ssh_keys = "${var.ssh_keys}"
 
   /* This can be optional, ugly as hell but it works */
   volume_ids = var.vol_size > 0 ? [digitalocean_volume.host[count.index].id] : []
@@ -55,23 +55,23 @@ resource "digitalocean_droplet" "host" {
       playbook {
         file_path = "${path.cwd}/ansible/bootstrap.yml"
       }
-      groups = ["${var.group}"]
+      groups = [var.group]
 
       extra_vars = {
         hostname         = "${var.name}-${format("%02d", count.index+1)}.${local.dc}.${var.env}.${local.stage}"
-        ansible_ssh_user = "${var.ssh_user}"
-        data_center      = "${local.dc}"
-        stage            = "${local.stage}"
-        env              = "${var.env}"
+        ansible_ssh_user = var.ssh_user
+        data_center      = local.dc
+        stage            = local.stage
+        env              = var.env
       }
     }
   }
 }
 
 resource "digitalocean_floating_ip" "host" {
-  droplet_id = "${element(digitalocean_droplet.host.*.id, count.index)}"
-  region     = "${element(digitalocean_droplet.host.*.region, count.index)}"
-  count      = "${var.host_count}"
+  droplet_id = element(digitalocean_droplet.host.*.id, count.index)
+  region     = element(digitalocean_droplet.host.*.region, count.index)
+  count      = var.host_count
   lifecycle {
     prevent_destroy = true
   }
@@ -91,36 +91,37 @@ resource "digitalocean_firewall" "host" {
 }
 
 resource "cloudflare_record" "host" {
-  domain = "${var.domain}"
-  count  = "${var.host_count}"
-  name   = "${element(digitalocean_droplet.host.*.name, count.index)}"
-  value  = "${element(digitalocean_floating_ip.host.*.ip_address, count.index)}"
+  domain = var.domain
+  count  = var.host_count
+  name   = element(digitalocean_droplet.host.*.name, count.index)
+  value  = element(digitalocean_floating_ip.host.*.ip_address, count.index)
   type   = "A"
   ttl    = 3600
 }
 
 /* combined dns entry for groups of hosts, example: nodes.do-ams3.thing.misc.statusim.net */
 resource "cloudflare_record" "hosts" {
-  domain = "${var.domain}"
+  domain = var.domain
   name   = "${var.name}s.${local.dc}.${var.env}.${local.stage}"
-  value  = "${element(digitalocean_floating_ip.host.*.ip_address, count.index)}"
-  count  = "${var.host_count}"
+  value  = element(digitalocean_floating_ip.host.*.ip_address, count.index)
+  count  = var.host_count
   type   = "A"
 }
 
 resource "ansible_host" "host" {
-  inventory_hostname = "${element(digitalocean_droplet.host.*.name, count.index)}"
-  groups = ["${var.group}", "${local.dc}"]
-  count = "${var.host_count}"
+  inventory_hostname = element(digitalocean_droplet.host.*.name, count.index)
+
+  groups = [var.group, local.dc]
+  count  = var.host_count
 
   vars = {
-    ansible_host = "${element(digitalocean_floating_ip.host.*.ip_address, count.index)}"
-    hostname     = "${element(digitalocean_droplet.host.*.name, count.index)}"
-    region       = "${element(digitalocean_droplet.host.*.region, count.index)}"
+    ansible_host = element(digitalocean_floating_ip.host.*.ip_address, count.index)
+    hostname     = element(digitalocean_droplet.host.*.name, count.index)
+    region       = element(digitalocean_droplet.host.*.region, count.index)
     dns_entry    = "${element(digitalocean_droplet.host.*.name, count.index)}.${var.domain}"
-    dns_domain   = "${var.domain}"
-    data_center  = "${local.dc}"
-    stage        = "${local.stage}"
-    env          = "${var.env}"
+    dns_domain   = var.domain
+    data_center  = local.dc
+    stage        = local.stage
+    env          = var.env
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 locals {
-  public_ips = "${digitalocean_floating_ip.host.*.ip_address}"
-  hostnames  = "${digitalocean_droplet.host.*.name}"
+  public_ips = digitalocean_floating_ip.host[*].ip_address
+  hostnames  = digitalocean_droplet.host[*].name
 }
 
 output "public_ips" {
-  value = ["${local.public_ips}"]
+  value = local.public_ips
 }
 
 output "hostnames" {
-  value = ["${local.hostnames}"]
+  value = local.hostnames
 }
 
 output "hosts" {
-  value = "${zipmap(local.hostnames, local.public_ips)}"
+  value = zipmap(local.hostnames, local.public_ips)
 }
