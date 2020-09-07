54b8c7b0a976f69eaa0ce6cd9b8939d4cf751f1b
Jakub Sokołowski <jakub@status.im>
2020-09-07 21:19:49 +0200

    upgrade to terraform 0.13
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- versions.tf
+++ versions.tf
@@ -1,8 +1,18 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
   required_providers {
-    digitalocean = "= 1.20.0"
-    cloudflare   = "= 2.3.0"
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "= 1.22.2"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "= 2.10.1"
+    }
+    ansible = {
+      source  = "nbering/ansible"
+      version = " = 1.0.4"
+    }
   }
 }
