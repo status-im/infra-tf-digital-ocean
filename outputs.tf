a48a750daabef38173f4507c4dbffd5d3443d8ed
Jakub Sokołowski <jakub@status.im>
2021-02-16 16:19:56 +0100

    add droplet IDs to outputs
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
 locals {
-  public_ips = digitalocean_floating_ip.host[*].ip_address
-  hostnames  = digitalocean_droplet.host[*].name
+  public_ips  = digitalocean_floating_ip.host[*].ip_address
+  hostnames   = digitalocean_droplet.host[*].name
+  droplet_ids = digitalocean_droplet.host[*].id
 }
 
 output "public_ips" {
@@ -13,4 +14,8 @@ output "hostnames" {
 
 output "hosts" {
   value = zipmap(local.hostnames, local.public_ips)
+}
+
+output "ids" {
+  value = zipmap(local.hostnames, local.droplet_ids )
 }
