534b2b2a2d796593bad2fc3bfca4b12df19aba14
Jakub Sokołowski <jakub@status.im>
2022-03-09 22:28:10 +0100

    upgrade DigitalOcean provider from 2.12.0 to 2.18.0
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "= 2.12.0"
+      version = "= 2.18.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
