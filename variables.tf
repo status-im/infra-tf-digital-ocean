71d83fad5f95c1491a757f344235a1f12a5975d5
markoburcul <marko@status.im>
2024-08-12 14:47:55 +0200

    variables: Add marko SSH public key ID
    Signed-off-by: markoburcul <marko@status.im>

--- variables.tf
+++ variables.tf
@@ -91,6 +91,7 @@ variable "ssh_keys" {
     "38154610", # alexis@status.im
     "39365941", # yakimant
     "42672671", # siddarth
+    "43047154", # markob
   ]
   /* cmd: doctl compute ssh-key list */
 }
